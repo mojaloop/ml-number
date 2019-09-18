f1cb204e2faac4cdc0ddec6b1f5f6bb2df18a9bd
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-09-18 19:33:02 +0200

    Initial commit for story #953 (#1)
    * Initial commit for numbers library
    
    * gitignore updated
    
    * updated tests
    
    * test refactor
    
    * added constructor function to create MlNumber object with multiple functions
    
    * added name to contributor list
    
    * standard changes
    
    * fixes for not allowing multiple functions to be strung together while still returning MlNumber object
    
    * added extra test
    
    * added to fixed functionality to format result with decimal places and rounding mode
    
    * fraction test
    
    * coverage fix and standard changes
    
    * updated version and reordered pre-commit
    made mlnumbers is a class again
    
    * reverted to constructor function still need to ensure idempotency
    
    * now idempotent
    
    * removed unnecessary toNumber calls
    
    * forgot to change sumList
    
    * Updated to class again and fixed README.md
    
    * added jsdocs
    
    * fix for param type in jsdoc
    
    * export the class not the file
    
    * Library can be required as ```require('@mojaloop/ml-number')``` instead of ```require('@mojaloop/ml-number').MlNumber```
    
    * added extra tests and more usage instructions
    
    * updated jsdocs for values that can be used in functions
    
    * fix index test

---
+++ README.md
@@ -0,0 +1,66 @@
+# ml-number
+[![Git Commit](https://img.shields.io/github/last-commit/mojaloop/ml-decimal.svg?style=flat)](https://github.com/mojaloop/ml-decimal/commits/master)
+[![Git Releases](https://img.shields.io/github/release/mojaloop/ml-decimal.svg?style=flat)](https://github.com/mojaloop/ml-decimal/releases)
+[![Npm Version](https://img.shields.io/npm/v/@mojaloop/ml-decimal.svg?style=flat)](https://www.npmjs.com/package/@mojaloop/ml-decimal)
+[![NPM Vulnerabilities](https://img.shields.io/snyk/vulnerabilities/npm/@mojaloop/ml-decimal.svg?style=flat)](https://www.npmjs.com/package/@mojaloop/ml-decimal)
+[![CircleCI](https://circleci.com/gh/mojaloop/ml-decimal.svg?style=svg)](https://circleci.com/gh/mojaloop/ml-decimal)
+
+Mojaloop Number Library implementation that handles decimal processing of amounts/numbers
+
+## Getting Started
+
+To use the ml-number library you need to add it as a dependency to your project with the following:
+```$xslt
+npm install @mojaloop/ml-number
+```
+
+### Usage
+
+To create a new instance of the MlNumber class(returns instance of MlNumber):
+```javascript 1.8
+const MlNumber = require('@mojaloop/ml-number')
+const mlNumber = new MlNumber(6) // can be instantiated with string or number of MlNumber
+const mlNumber2 = new MlNumber('6') // can be instantiated with string or number of MlNumber
+const mlNumber3 = new MlNumber(mlNumber2) // can be instantiated with string or number of MlNumber
+```
+
+Use different function(returns new instance of new MlNumber):
+```javascript 1.8
+const MlNumber = require('@mojaloop/ml-number')
+
+const originalValue = new MlNumber(6) // = "{"MlNumber": "6"}"
+
+const additionResult = originalValue.add(12) // = "{"MlNumber": "18"}"
+
+const multiplyResult = originalValue.multiply('12') // = "{"MlNumber": "72"}"
+
+const divisionResult = originalValue.divide(2) // = "{"MlNumber": "3"}"
+
+const subtractionResult = originalValue.subtract('2') // = "{"MlNumber": "4"}"
+
+const mixedFunctionsValuesResult = new MlNumber('2').add(originalValue).sumList([additionResult, 5]) // = "{"MlNumber": "31"}"
+
+const differentFunctionsResult = originalValue.add(2).multiply(3).divide(2).subtract(2) // = "{"MlNumber": "10"}"
+
+const sumListResult = originalValue.sumList([2,3]) // = "{"MlNumber": "11"}"
+
+const toStringResult = additionResult.toString() // = "18"
+
+const toNumberResult = multiplyResult.toNumber() // = 72
+
+const toFixedResult = divisionResult.toFixed() // = "3.0000"
+
+const toFixedWithDecimalPlaceResult = subtractionResult.toFixed(7) // = "4.0000000"
+```
+
+## Running the tests
+
+To run the unit tests locally 
+```$xslt
+npm test
+```
+
+To run the test coverage tests locally 
+```$xslt
+npm run test:coverage
+```
---
+++ test/unit/mlnumber.test.js
@@ -0,0 +1,168 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ * Name Surname <name.surname@gatesfoundation.com>
+
+ * Rajiv Mothilal <rajiv.mothilal@modusbox.com>
+ --------------
+ ******/
+'use strict'
+
+const Test = require('tapes')(require('tape'))
+const MlNumber = require('../../src')
+
+Test('MlNumber', mlNumberTest => {
+  mlNumberTest.test('sumList should add a list of numbers', test => {
+    const sum = new MlNumber().sumList([1, 2, 3])
+    test.equal(sum.toString(), '6')
+    test.end()
+  })
+
+  mlNumberTest.test('sumList should add a list of string numbers', test => {
+    const sum = new MlNumber().sumList(['1', '2', '3'])
+    test.equal(sum.toString(), '6')
+    test.end()
+  })
+
+  mlNumberTest.test('sumList should add a list of string numbers and normal numbers', test => {
+    const sum = new MlNumber().sumList(['1', 2, '3'])
+    test.equal(sum.toNumber(), 6)
+    test.end()
+  })
+
+  mlNumberTest.test('add should add 2 numbers', test => {
+    const add = new MlNumber(1).add(2)
+    test.equal(add.toString(), '3')
+    test.end()
+  })
+
+  mlNumberTest.test('add should add 2 string numbers', test => {
+    const add = new MlNumber('1').add('2')
+    test.equal(add.toString(), '3')
+    test.end()
+  })
+
+  mlNumberTest.test('add should add 2 string numbers and return a number', test => {
+    const add = new MlNumber('1').add('2')
+    test.equal(add.toNumber(), 3)
+    test.end()
+  })
+
+  mlNumberTest.test('add should add a string number and number', test => {
+    const add = new MlNumber('1').add(2)
+    test.equal(add.toString(), '3')
+    test.end()
+  })
+
+  mlNumberTest.test('multiply should multiply 2 numbers', test => {
+    const multiply = new MlNumber(12).multiply(2)
+    test.equal(multiply.toNumber(), 24)
+    test.end()
+  })
+
+  mlNumberTest.test('multiply should multiply 2 string numbers', test => {
+    const multiply = new MlNumber('12').multiply('2')
+    test.equal(multiply.toString(), '24')
+    test.end()
+  })
+
+  mlNumberTest.test('multiply should multiply a string number and normal number', test => {
+    const multiply = new MlNumber('12').multiply(2)
+    test.equal(multiply.toString(), '24')
+    test.end()
+  })
+
+  mlNumberTest.test('subtract should subtract 2 numbers', test => {
+    const difference = new MlNumber(10).subtract(2)
+    test.equal(difference.toNumber(), 8)
+    test.end()
+  })
+
+  mlNumberTest.test('subtract should subtract 2 string numbers', test => {
+    const difference = new MlNumber('10').subtract('2')
+    test.equal(difference.toString(), '8')
+    test.end()
+  })
+
+  mlNumberTest.test('subtract should subtract a string number and normal number', test => {
+    const difference = new MlNumber('10').subtract(2)
+    test.equal(difference.toString(), '8')
+    test.end()
+  })
+
+  mlNumberTest.test('divide should divide 2 numbers', test => {
+    const divide = new MlNumber(1).divide(2)
+    test.equal(divide.toNumber(), 0.5)
+    test.end()
+  })
+
+  mlNumberTest.test('divide should divide 2 string numbers', test => {
+    const divide = new MlNumber('1').divide('2')
+    test.equal(divide.toString(), '0.5')
+    test.end()
+  })
+
+  mlNumberTest.test('divide should divide a string number and normal number', test => {
+    const divide = new MlNumber('1').divide(2)
+    test.equal(divide.toString(), '0.5')
+    test.end()
+  })
+
+  mlNumberTest.test('all functions should be able to be strung together', test => {
+    const total = new MlNumber('2').add(10)
+    const newTotal = total.multiply(5).subtract(10).divide(2).sumList([2, 3])
+    test.equal(newTotal.toString(), '30')
+    test.end()
+  })
+
+  mlNumberTest.test('all functions should be able to be strung together but original value must be unchanged', test => {
+    const total = new MlNumber('2').add(10)
+    const newTotal = total.multiply(5).subtract(10).divide(2).sumList([2, 3])
+    test.equal(newTotal.toString(), '30')
+    test.equal(total.toString(), '12')
+    test.end()
+  })
+
+  mlNumberTest.test('should take in string and be able to represent it as a number', test => {
+    const mlNumber = new MlNumber('1')
+    test.equal(mlNumber.toNumber(), 1)
+    test.end()
+  })
+
+  mlNumberTest.test('toFixed should format the MlNumber to the supplied decimal place and rounding mode', test => {
+    const mlNumber = new MlNumber('0.1').add(0.2)
+    test.equal(mlNumber.toFixed(), '0.3000')
+    test.end()
+  })
+
+  mlNumberTest.test('Do calculations with strings number and MlNumbers', test => {
+    const mlNumber = new MlNumber(9)
+    const total = new MlNumber('2').add(mlNumber).sumList([mlNumber, 5])
+    test.equal(total.toString(), '25')
+    test.end()
+  })
+
+  mlNumberTest.test('Create MlNumber with MlNumber variable', test => {
+    const mlNumber = new MlNumber(9)
+    const mlNumberMlNumber = new MlNumber(mlNumber)
+    test.deepEqual(mlNumber, mlNumberMlNumber, 'Ml number created from MlNumber are equal')
+    test.end()
+  })
+
+  mlNumberTest.end()
+})
